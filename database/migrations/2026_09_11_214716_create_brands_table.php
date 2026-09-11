7d0c0e89d34c049c18e48ebfa43da91610a65a45
Muhammad Talha <117847229+imiantalha@users.noreply.github.com>
2026-09-11 15:19:43 +0500

    feat: complete brands migration

--- database/migrations/2026_09_11_214716_create_brands_table.php
+++ database/migrations/2026_09_11_214716_create_brands_table.php
@@ -6,26 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('brands', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('slug')->unique();
-            $table->string('logo')->nullable();
             $table->text('description')->nullable();
-
             $table->enum('status', ['active', 'inactive'])->default('active');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('brands');
