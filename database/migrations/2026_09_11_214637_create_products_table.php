aa2268ce7aa01d3e9377ac12945a6c703581e9ee
Muhammad Talha <117847229+imiantalha@users.noreply.github.com>
2026-09-11 15:19:37 +0500

    feat: complete products migration

--- database/migrations/2026_09_11_214637_create_products_table.php
+++ database/migrations/2026_09_11_214637_create_products_table.php
@@ -6,20 +6,27 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('brand_id')->nullable();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->string('sku')->nullable()->unique();
+            $table->enum('type', ['simple', 'variable']);
+            $table->string('short_description')->nullable();
+            $table->text('description')->nullable();
+            $table->decimal('base_price', 12, 2)->nullable();
+            $table->decimal('compare_at_price', 12, 2)->nullable();
+            $table->decimal('cost_price', 12, 2)->nullable();
+            $table->enum('status', ['draft', 'active', 'inactive', 'archived'])->default('draft');
+            $table->unsignedBigInteger('feature_media_id')->nullable();
+            $table->json('metadata')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('products');
