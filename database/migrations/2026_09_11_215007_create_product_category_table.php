ca899b1232d5c4441b66f88d7633dd0a157f1256
Muhammad Talha <117847229+imiantalha@users.noreply.github.com>
2026-09-11 15:20:06 +0500

    feat: complete product category pivot

--- database/migrations/2026_09_11_215007_create_product_category_table.php
+++ database/migrations/2026_09_11_215007_create_product_category_table.php
@@ -6,20 +6,15 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('product_category', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->foreignId('product_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            $table->primary(['product_id', 'category_id']);
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('product_category');
