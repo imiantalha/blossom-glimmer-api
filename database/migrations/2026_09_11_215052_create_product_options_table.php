e6cf7042f5151c2a86d457cfad03a9c6a0a5dde9
Muhammad Talha <117847229+imiantalha@users.noreply.github.com>
2026-09-11 15:20:11 +0500

    feat: complete product options migration

--- database/migrations/2026_09_11_215052_create_product_options_table.php
+++ database/migrations/2026_09_11_215052_create_product_options_table.php
@@ -6,20 +6,17 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('product_options', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('product_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('attribute_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
+            $table->unique(['product_id', 'attribute_id']);
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('product_options');
