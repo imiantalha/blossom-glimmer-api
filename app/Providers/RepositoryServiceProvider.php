36572c8afa0646f027803711966fbe079291bea7
Muhammad Talha <117847229+imiantalha@users.noreply.github.com>
2026-09-14 15:36:15 +0500

    feat: bind product repository

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -7,42 +7,24 @@ use App\Repositories\UserRepository;
 use App\Repositories\RoleRepository;
 use App\Repositories\DashboardRepository;
 use App\Repositories\PermissionRepository;
+use App\Repositories\ProductRepository;
 use App\Repositories\Contracts\UserRepositoryInterface;
 use App\Repositories\Contracts\RoleRepositoryInterface;
 use App\Repositories\Contracts\DashboardRepositoryInterface;
 use App\Repositories\Contracts\PermissionRepositoryInterface;
+use App\Repositories\Contracts\ProductRepositoryInterface;
 
 class RepositoryServiceProvider extends ServiceProvider
 {
-    /**
-     * Register services.
-     */
     public function register(): void
     {
-        $this->app->bind(
-            DashboardRepositoryInterface::class,
-            DashboardRepository::class
-        );
-
-        $this->app->bind(
-            UserRepositoryInterface::class,
-            UserRepository::class
-        );
-
-        $this->app->bind(
-            RoleRepositoryInterface::class,
-            RoleRepository::class
-        );
-
-        $this->app->bind(
-            PermissionRepositoryInterface::class,
-            PermissionRepository::class
-        );
+        $this->app->bind(DashboardRepositoryInterface::class, DashboardRepository::class);
+        $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
+        $this->app->bind(RoleRepositoryInterface::class, RoleRepository::class);
+        $this->app->bind(PermissionRepositoryInterface::class, PermissionRepository::class);
+        $this->app->bind(ProductRepositoryInterface::class, ProductRepository::class);
     }
 
-    /**
-     * Bootstrap services.
-     */
     public function boot(): void
     {
         //
