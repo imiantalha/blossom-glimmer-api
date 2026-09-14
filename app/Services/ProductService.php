149cf8e9fe6d767f468d56918930472b83694d24
Muhammad Talha <117847229+imiantalha@users.noreply.github.com>
2026-09-14 15:35:54 +0500

    feat: add product service

---
+++ app/Services/ProductService.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Product;
+use App\Repositories\Contracts\ProductRepositoryInterface;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Facades\DB;
+
+class ProductService
+{
+    public function __construct(
+        protected ProductRepositoryInterface $productRepository
+    ) {
+    }
+
+    public function paginate(array $filters): LengthAwarePaginator
+    {
+        return $this->productRepository->paginate($filters);
+    }
+
+    public function find(Product $product): Product
+    {
+        return $this->productRepository->find($product);
+    }
+
+    public function create(array $data): Product
+    {
+        return DB::transaction(function () use ($data) {
+            $categoryIds = $data['category_ids'] ?? [];
+            $tagIds = $data['tag_ids'] ?? [];
+
+            unset($data['category_ids'], $data['tag_ids']);
+
+            $product = $this->productRepository->create($data);
+            $product->categories()->sync($categoryIds);
+            $product->tags()->sync($tagIds);
+
+            return $this->productRepository->find($product);
+        });
+    }
+
+    public function update(Product $product, array $data): Product
+    {
+        return DB::transaction(function () use ($product, $data) {
+            if (array_key_exists('category_ids', $data)) {
+                $product->categories()->sync($data['category_ids']);
+                unset($data['category_ids']);
+            }
+
+            if (array_key_exists('tag_ids', $data)) {
+                $product->tags()->sync($data['tag_ids']);
+                unset($data['tag_ids']);
+            }
+
+            $product = $this->productRepository->update($product, $data);
+
+            return $this->productRepository->find($product);
+        });
+    }
+
+    public function delete(Product $product): bool
+    {
+        return DB::transaction(function () use ($product) {
+            $product->categories()->detach();
+            $product->tags()->detach();
+
+            return $this->productRepository->delete($product);
+        });
+    }
+}
